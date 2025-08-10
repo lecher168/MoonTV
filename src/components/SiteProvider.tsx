2cb84a4756e93398e109aafecc38969d643f74df
lecher168 <88260942+lecher168@users.noreply.github.com>
2025-08-11 02:13:41 +0800

    Update SiteProvider.tsx

--- src/components/SiteProvider.tsx
+++ src/components/SiteProvider.tsx
@@ -4,9 +4,9 @@ import { createContext, ReactNode, useContext } from 'react';
 
 const SiteContext = createContext<{ siteName: string; announcement?: string }>({
   // 默认值
-  siteName: 'MoonTV',
+  siteName: '可搭建你的私人影院',
   announcement:
-    '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+    '本项目为：粉丝群团体、公司员工、家族成员、村组及小区等...福利开发，价格低廉。项目咨询合作：<img src="https://wangpan.szai.us.kg/file/1754239378387_微信图片_20250710031229.jpg" alt="微信图片_20250710031229.jpg" width=100%>',
 });
 
 export const useSite = () => useContext(SiteContext);
