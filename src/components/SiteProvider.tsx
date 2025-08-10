abc23c8a84ddfe054eea6b6c70ac010995b25d01
lecher168 <88260942+lecher168@users.noreply.github.com>
2025-08-11 02:18:39 +0800

    Update SiteProvider.tsx

--- src/components/SiteProvider.tsx
+++ src/components/SiteProvider.tsx
@@ -6,7 +6,7 @@ const SiteContext = createContext<{ siteName: string; announcement?: string }>({
   // 默认值
   siteName: '可搭建你的私人影院',
   announcement:
-    '本项目为：粉丝群团体、公司员工、家族成员、村组及小区等...福利开发，价格低廉。项目咨询合作：<img src="https://wangpan.szai.us.kg/file/1754239378387_微信图片_20250710031229.jpg" alt="微信图片_20250710031229.jpg" width=100%>',
+    '本项目为：粉丝群团体、公司员工、家族成员、村组及小区等...福利开发，价格低廉。项目咨询合作：@色者AI。',
 });
 
 export const useSite = () => useContext(SiteContext);
